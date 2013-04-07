0e3a43bbe8139c589f9683dcad35ffddf2ab49d9
Matt Schwartz <matt_dev1@hotmail.com>
2013-04-07 16:00:37 -0700

    Merging with my remote work

--- src/ARR.Prototype.MVC/Global.asax.cs
+++ src/ARR.Prototype.MVC/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ARR.Prototype.MVC.Notifications;
+using Raven.Abstractions.Data;
 using Raven.Client.Embedded;
 using Raven.Database.Server;
 
@@ -37,7 +39,29 @@ namespace ARR.Prototype.MVC
 
             documentStore.Initialize();
 
+            documentStore.Changes()
+                .ForAllDocuments()
+                .Subscribe(new ChangeObserver());
+
             Application["raven"] = documentStore;
+        }
+    }
+
+    public class ChangeObserver : IObserver<DocumentChangeNotification>
+    {
+        public void OnCompleted()
+        {
+            new NotificationSender().Send();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(DocumentChangeNotification value)
+        {
+            new NotificationSender().Send();
         }
     }
 }
