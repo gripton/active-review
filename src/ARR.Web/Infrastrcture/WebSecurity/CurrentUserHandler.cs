bb907a7703029503af56ee234bc128d5d723d9ed
Matt Schwartz <thorfio@gmail.com>
2013-04-30 21:01:54 -0700

    Task 3227: Adding Current User Handler

---
+++ src/ARR.Web/Infrastrcture/WebSecurity/CurrentUserHandler.cs
@@ -0,0 +1,22 @@
+﻿using ARR.Web.Infrastructure;
+using ARR.Web.Infrastructure.WebSecurity;
+using PracticalCode.WebSecurity.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ARR.Web.Infrastrcture
+{
+    public class CurrentUserHandler : JsonRequestHandler    {     
+
+        public IWebApplicationServices WebApplicationServices { get; set; }
+        
+        public override void ProcessRequest(HttpContext context)
+        {
+            var user = WebApplicationServices.WebSecurity.GetCurrentUser();
+            SetResponse<WebSecurityUser>(context, user);            
+        }
+
+    }
+}
