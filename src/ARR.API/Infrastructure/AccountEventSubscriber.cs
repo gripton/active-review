b438c124d7f1f6e2f0bf601f34090a713aa5c303
Matt Schwartz <matt_dev1@hotmail.com>
2013-05-06 20:21:24 -0700

    Feature 3229: Ordering the ReviewIndex Get Call

--- src/ARR.API/Infrastructure/AccountEventSubscriber.cs
+++ src/ARR.API/Infrastructure/AccountEventSubscriber.cs
@@ -2,10 +2,6 @@
 using ARR.Repository;
 using Autofac;
 using Raven.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace ARR.API.Infrastructure
 {
--- src/ARR.IntegrationTests/API/ApplicationModule.cs
+++ src/ARR.IntegrationTests/API/ApplicationModule.cs
@@ -61,8 +61,8 @@ namespace ARR.IntegrationTests.API
             builder
                 .Register(c => new DocumentStore
                 {
-                    Url = "https://2.ravenhq.com/databases/AppHarbor_6d9ac094-3ec0-454d-8f3f-b838f0847e99",
-                    ApiKey = "8145df81-9552-4979-b44a-7aaf671ddbce"
+                    Url = "https://aeo.ravenhq.com/databases/AppHarbor_48e97815-70ea-43bc-ac81-4229e1cc4454",
+                    ApiKey = "3d9f210f-2fdc-4eb5-a350-5d4cc3a1e226"
                 })
                 .As<IDocumentStore>()
                 .SingleInstance()
