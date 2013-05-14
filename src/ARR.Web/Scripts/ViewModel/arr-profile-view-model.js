a6b9a378097ab93efe6ae23de6cb130b2456688e
tjochums <tjochums@pdx.edu>
2013-05-13 22:43:42 -0700

    Issue 3264: Got the profile page updating

--- src/ARR.Web/Scripts/ViewModel/arr-profile-view-model.js
+++ src/ARR.Web/Scripts/ViewModel/arr-profile-view-model.js
@@ -1,14 +1,14 @@
-﻿var ProfileViewModel = function() {
+﻿var ProfileViewModel = function () {
 
     var self = this;
     self.accountId = ko.observable();
     self.account = new Account();
     self.websecurityUser = new Account();
-   
+
     self.updateProfile = function () {
         $.ajax({
             type: "PUT",
-            url: getArrApiUrlPost('account/' + self.reviewSessionId),
+            url: getArrApiUrlPost('account/' + self.account.Id() + '/account'),
             data: ko.toJSON(self.account),
             contentType: 'application/json',
             dataType: 'JSON',
@@ -18,29 +18,28 @@
         });
     };
 
-    self.changePassword = function() {
+    self.changePassword = function () {
         $.ajax({
             type: "POST",
             url: "http://localhost:49882/api/account",
             data: ko.toJSON(this.Data),
             contentType: 'application/json',
             dataType: 'JSON',
-            success: function(data) { $('#myModal').modal('show'); }
+            success: function (data) { $('#myModal').modal('show'); }
         });
     };
-    
+
     self.load = function () {
         ko.applyBindings(self);
-        
+
         $.getJSON('user.user', function (allData) {
             ko.mapping.fromJS(allData, {}, self.websecurityUser);
             console.log(ko.toJSON(self.websecurityUser));
-        });
-
-        $.getJSON(getArrApiUrl('account/' + self.websecurityUser.Id()), function (allData) {
+            $.getJSON(getArrApiUrl('account/' + self.websecurityUser.Username()), function (allData) {
                 ko.mapping.fromJS(allData, {}, self.account);
-                console.log(ko.toJSON(self.account));
+                console.log(self.account.ScreenName());
             });
+        });
     };
 
     self.load();
