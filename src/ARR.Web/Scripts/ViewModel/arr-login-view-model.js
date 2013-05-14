b0258e14b24b33be87472e199fd3d57f652cfa56
Matt Schwartz <matt_dev1@hotmail.com>
2013-05-13 21:51:23 -0700

    More team meeting work

---
+++ src/ARR.Web/Scripts/ViewModel/arr-login-view-model.js
@@ -0,0 +1,26 @@
+﻿var LoginViewModel = function() {
+
+    var self = this;
+    self.account = new Account();
+    
+    self.login = function() {
+        $.ajax({
+            type: "POST",
+            url: "login.login",
+            data: ko.toJSON(this.account),
+            contentType: 'application/json',
+            dataType: 'JSON',
+            success: function(response) {
+                location.href = "home.html";
+            },
+            error: function(response) {
+                alert("Ajax Request Failed");
+                //this.showError(response);
+                //completeFunction();
+            }
+        });
+    };
+};
+
+ko.applyBindings(new LoginViewModel());
+
--- src/ARR.Web/Scripts/ViewModel/arr-profile-view-model.js
+++ src/ARR.Web/Scripts/ViewModel/arr-profile-view-model.js
@@ -1,21 +1,10 @@
-﻿var AccountViewModel = function() {
+﻿var ProfileViewModel = function() {
 
     var self = this;
-    self.accountId = ko.observable()
+    self.accountId = ko.observable();
     self.account = new Account();
     self.websecurityUser = new Account();
-
-    self.create = function() {
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:49882/api/account",
-            data: ko.toJSON(self.account),
-            contentType: 'application/json',
-            dataType: 'JSON',
-            success: function(data) { $('#myModal').modal('show'); }
-        });
-    };
-    
+   
     self.updateProfile = function () {
         $.ajax({
             type: "PUT",
@@ -39,24 +28,6 @@
             success: function(data) { $('#myModal').modal('show'); }
         });
     };
-
-    self.login = function() {
-        $.ajax({
-            type: "POST",
-            url: "login.login",
-            data: ko.toJSON(this.Data),
-            contentType: 'application/json',
-            dataType: 'JSON',
-            success: function(response) {
-                location.href = "home.html";
-            },
-            error: function(response) {
-                alert("Ajax Request Failed");
-                //this.showError(response);
-                //completeFunction();
-            }
-        });
-    };
     
     self.load = function () {
         ko.applyBindings(self);
@@ -76,5 +47,5 @@
 };
 
 
-var accountViewModel = new AccountViewModel();
+var profileViewModel = new ProfileViewModel();
 
