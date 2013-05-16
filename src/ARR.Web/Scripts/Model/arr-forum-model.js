cc73ecdfb12c94795a2c0d7c3458e599172f2893
tjochums <tjochums@pdx.edu>
2013-05-15 21:46:51 -0700

    Issue 3261: Modify the forum page to pull its data and push it up to the server (Chat is still unhooked)

--- src/ARR.Web/Scripts/Model/arr-forum-model.js
+++ src/ARR.Web/Scripts/Model/arr-forum-model.js
@@ -1,55 +1,19 @@
-﻿function Question(questionText) {
-    var self = this;
-    self.questionText = questionText;
-    self.answer = ko.observable("Your answer Here")
-    self.Feedback = ko.observableArray([
-        new Feedback("OriginalFeedback", "Tom", Date.now())
-    ]);
-
-    self.newFeedback = ko.observable("");
-
-    self.selectItem = function (data) {
-        requirementsModel.setFeedback(data);
-    }
-}
-
-function Feedback(feedback, userName, time) {
-    var self = this;
-    self.feedback = feedback;
-    self.user = userName;
-    self.time = time;
-}
-
-function ChatMessage(message, userName, time) {
+﻿function ChatMessage(message, userName, time) {
     var self = this;
     self.message = message;
     self.user = userName;
     self.time = time;
-}
-
-// Here's my data model
-var ViewModel = function () {
-    var self = this;
-    self.selectedItem = null;
-
-    self.questionList = ko.observableArray([
-        new Question("This is Question #1"),
-        new Question("This is Question #2"),
-        new Question("This is Question #3")
-    ]);
-
-    self.ChatMessages = ko.observableArray([
-        new ChatMessage("Here are my initial Comments", "Tom", Date.now()),
-        new ChatMessage("And some friendly chit chat to follow that up", "Dan", Date.now())
-    ]);
-
-    self.saveFeedback = function () {
-        self.selectedItem.Feedback().push(new Feedback(self.currentFeedback(),"Tom", Date.now()));
-        self.selectedItem = null;
-    }
-
-    self.newMessage = ko.observable("");
 };
 
-var requirementsModel = new ViewModel();
-ko.applyBindings(requirementsModel); // This makes Knockout get to work
+function Feedback() {
+    var self = this;
+    self.Username = ko.observable();
+    self.Text = ko.observable();
+    self.Created = Date.now();
+};
+
+function QuestionFeedback(question) {
+    var self = this;
+    self.Question = question;
+    self.NewFeedback = ko.observable();
+}
---
+++ src/ARR.Web/Scripts/ViewModel/arr-forum-view-model.js
@@ -0,0 +1,70 @@
+﻿// Here's my data model
+var ForumViewModel = function (reviewSessionId) {
+    var self = this;
+    self.reviewSessionId = reviewSessionId;
+    self.reviewSession = new ReviewSession();
+    self.questionList = ko.observableArray();
+    self.addFeedbackViewModel = new AddFeedbackViewModel(self);
+
+    self.ChatMessages = ko.observableArray([
+        new ChatMessage("Here are my initial Comments", "Tom", Date.now()),
+        new ChatMessage("And some friendly chit chat to follow that up", "Dan", Date.now())
+    ]);
+
+    self.newMessage = ko.observable("");
+    
+    self.load = function () {
+        //Need to set up the dirty flag for the review session now so it is fresh
+        ko.applyBindings(self); // This makes Knockout get to work
+
+        if (self.reviewSessionId != null) {
+            //self.isLoading(true);
+
+            $.getJSON(getArrApiUrl('reviewsession/' + self.reviewSessionId), function (allData) {
+                ko.mapping.fromJS(allData, {}, self.reviewSession);
+
+                for (var k = 0; k < self.reviewSession.Questions().length; k++) {
+                    self.questionList.push(new QuestionFeedback(self.reviewSession.Questions()[k]));
+                }
+                setScrollDisplay("Left");
+                //self.isLoading(false);
+            });
+        }
+    };
+
+    // Load the review Session
+    self.load();
+};
+
+var AddFeedbackViewModel = function(forumViewModel) {
+    var self = this;
+    self.forumViewModel = forumViewModel;
+
+    self.saveFeedback = function (selectedQuestion) {
+        var f = new Feedback();
+        f.Text(selectedQuestion.NewFeedback());
+        //f.Username = self
+
+        if (selectedQuestion.Question.Feedbacks() == null) {
+            selectedQuestion.Question.Feedbacks([]);
+        }
+        selectedQuestion.Question.Feedbacks.push(f);
+
+        $.ajax(getArrApiUrlPost('feedback/' + self.forumViewModel.reviewSessionId + '/feedback'), {
+            data: ko.toJSON(selectedQuestion.Question),
+            dataType: "json",
+            type: "post",
+            contentType: "application/json",
+            success: function (result) {
+                alert(result);
+                // Clear out the feedback text box that we just added
+                selectedQuestion.NewFeedback('');
+                setScrollDisplay("Left");
+            }
+        });
+    };
+};
+
+
+var reviewSessionId = $.url(window.location).param('reviewSession');
+var forumModel = new ForumViewModel(reviewSessionId);
