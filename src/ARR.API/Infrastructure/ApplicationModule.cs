3aa1c65a8e0774a9a2d1f04447bffb786c6af920
tjochums <tjochums@pdx.edu>
2013-05-16 21:12:16 -0700

    Issue 3233: Pulled the RavenDB values out into the web.config so we can set it up separately on the server if we need to point to another one at some point.

--- src/ARR.API/Infrastructure/ApplicationModule.cs
+++ src/ARR.API/Infrastructure/ApplicationModule.cs
@@ -1,4 +1,5 @@
-﻿using ARR.AccountManagement;
+﻿using System.Configuration;
+using ARR.AccountManagement;
 using ARR.API.Infrastructure;
 using ARR.Notifications;
 using ARR.Repository;
@@ -68,8 +69,8 @@ namespace ARR.API.Controllers
             builder
                 .Register(c => new DocumentStore
                 {
-                    Url = "https://2.ravenhq.com/databases/AppHarbor_6d9ac094-3ec0-454d-8f3f-b838f0847e99",
-                    ApiKey = "8145df81-9552-4979-b44a-7aaf671ddbce"
+                    Url = ConfigurationManager.AppSettings["RavenUrl"],
+                    ApiKey = ConfigurationManager.AppSettings["RavenKey"]
                 })
                 .As<IDocumentStore>()
                 .SingleInstance()
--- src/ARR.API/Infrastructure/BackgroundModule.cs
+++ src/ARR.API/Infrastructure/BackgroundModule.cs
@@ -1,4 +1,5 @@
-﻿using ARR.AccountManagement;
+﻿using System.Configuration;
+using ARR.AccountManagement;
 using ARR.API.Infrastructure;
 using ARR.Data.Entities;
 using ARR.Notifications;
@@ -40,8 +41,8 @@ namespace ARR.API.Controllers
             builder
                 .Register(c => new DocumentStore
                 {
-                    Url = "https://2.ravenhq.com/databases/AppHarbor_6d9ac094-3ec0-454d-8f3f-b838f0847e99",
-                    ApiKey = "8145df81-9552-4979-b44a-7aaf671ddbce"
+                    Url = ConfigurationManager.AppSettings["RavenUrl"],
+                    ApiKey = ConfigurationManager.AppSettings["RavenKey"]
                 })
                 .As<IDocumentStore>()
                 .SingleInstance()
