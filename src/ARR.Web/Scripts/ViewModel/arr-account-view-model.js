452f7b891610c8df44542a1dce33b51666f71781
tjochums <tjochums@pdx.edu>
2013-05-27 18:45:27 -0700

    Added the Processing dialog to the Account Creation screen.

--- src/ARR.Web/Scripts/ViewModel/arr-account-view-model.js
+++ src/ARR.Web/Scripts/ViewModel/arr-account-view-model.js
@@ -1,16 +1,21 @@
 ﻿var AccountViewModel = function() {
-
     var self = this;
     self.account = new Account();
 
-    self.create = function() {
+    self.processingViewModel = new ProcessingViewModel();
+
+    self.create = function () {
+        self.processingViewModel.turnOnProcessing("Creating Account");
         $.ajax({
             type: "POST",
             url: getArrApiUrlPost('account/'),
             data: ko.toJSON(this.account),
             contentType: 'application/json',
             dataType: 'JSON',
-            success: function(data) { $('#myModal').modal('show'); }
+            success: function () {
+                self.processingViewModel.turnOffProcessing();
+                $('#myModal').modal('show');
+            }
         });
     };
 };
