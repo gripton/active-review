072353f93d7f423cd9fb1589ff8941572c18d99b
Matt Schwartz <matt_dev1@hotmail.com>
2013-05-30 19:46:32 -0700

    Trying to get a background worker running

---
+++ src/ARR.Background/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NLog;
+
+
+
+namespace ARR.Background
+{
+    class Program
+    {
+        private static readonly Logger log = LogManager.GetLogger(typeof(Program).Name);
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Starting - hit any key to continue...");
+
+            log.Debug("The background thing actually worked!");
+            
+            Console.ReadLine();
+
+            Console.WriteLine("Ending");
+        }
+    }
+}
