ef7eafba0b123e8b11080aec1eadfdab0e4348fe
Matt Schwartz <matt_dev1@hotmail.com>
2013-06-02 16:20:28 -0700

    Build Fix

---
+++ src/ARR.Background/BackgroundModule.cs
@@ -0,0 +1,64 @@
+﻿using ARR.AccountManagement;
+using ARR.Notifications;
+using ARR.Repository;
+using ARR.ReviewSessionManagement;
+using Autofac;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace ARR.API.Controllers
+{
+    public class BackgroundModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+           //builder
+           //     .RegisterType<NotificationGenerator>()
+           //     .As<INotificationGenerator>()
+           //     .InstancePerLifetimeScope();
+
+           // builder
+           //     .RegisterType<NotificationSender>()
+           //     .As<INotificationSender>()
+           //     .InstancePerLifetimeScope();     
+
+           // builder
+           //     .Register(c => new DocumentStore
+           //     {
+           //         Url = ConfigurationManager.AppSettings["RavenUrl"],
+           //         ApiKey = ConfigurationManager.AppSettings["RavenKey"]
+           //     })
+           //     .As<IDocumentStore>()
+           //     .SingleInstance()
+           //     .OnActivating(c => c.Instance.Initialize());
+
+
+            builder
+                .Register(c => c.Resolve<IDocumentStore>().OpenSession())
+                .As<IDocumentSession>()
+                .InstancePerLifetimeScope();
+
+            builder
+                .RegisterType<AccountRepository>()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+
+            builder
+                .RegisterType<ReviewSessionRepository>()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+
+            builder
+                .RegisterType<AccountMonitor>()
+                .As<IAccountMonitor>()
+                .InstancePerLifetimeScope();
+
+            builder
+                .RegisterType<ReviewSessionMonitor>()
+                .As<IReviewSessionMonitor>();    
+                
+        }
+    }
+
+}
---
+++ src/ARR.Background/Events/EventPublisher.cs
@@ -0,0 +1,29 @@
+﻿using ARR.Data.Entities;
+using ARR.Repository;
+
+using Quartz;
+using Raven.Client;
+
+namespace ARR.Background.Events
+{
+    public class EventPublisher : Publisher<Event>, IJob
+    {
+        private readonly IDocumentStore _store;
+
+        public EventPublisher(IDocumentStore store)
+        {
+            _store = store;
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            var eventRepository = new EventRepository(_store.OpenSession());
+            var events = eventRepository.FindAll();
+
+            foreach (var evt in events)
+            {
+                Notify(evt);
+            }
+        }
+    }
+}
